24967191ce44dd89a0b744cc746f6a530efd8c81
reddomeuk <academy@reddome.org>
2025-05-03 22:00:45 +0100

    rls19

--- terraform/modules/device_posture/main.tf
+++ terraform/modules/device_posture/main.tf
@@ -86,21 +86,3 @@ resource "cloudflare_zero_trust_device_posture_rule" "firewall_check" {
   
   depends_on = [cloudflare_zero_trust_device_posture_integration.intune_integration]
 }
-
-# Domain Join Check - with input block added
-resource "cloudflare_zero_trust_device_posture_rule" "domain_joined_check" {
-  account_id  = var.account_id
-  name        = "Domain Joined Check"
-  description = "Ensure device is domain joined"
-  type        = "domain_joined"
-  
-  match {
-    platform = "windows"
-  }
-  
-  input {
-    domain_names = ["*"]  # Match any domain
-  }
-  
-  depends_on = [cloudflare_zero_trust_device_posture_integration.intune_integration]
-}
